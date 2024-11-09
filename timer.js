fd296dba35f9fb78b65a9fc1f5a4db5039b3b293
ZIKE <36049864+hzijad@users.noreply.github.com>
2024-11-09 18:33:51 +0100

    Code for general timer functionality

--- timer.js
+++ timer.js
@@ -1,1 +1,49 @@
+class Timer{
 
+    constructor(){
+        this.starttime = 0;
+        this.stoptime = 0;
+        this.running = false;
+        this.elapsed = 0;
+    }
+    start(){
+        this.starttime = Date.now();
+        this.running = true;
+    }
+
+    stop(){
+    this.stoptime = Date.now();
+    this.running = false;
+    this.elapsed += this.stoptime - this.starttime;
+    }
+
+    reset(){
+        this.starttime = 0;
+        this.stoptime = 0;
+        this.running = false;
+        this.elapsed = 0;
+    }
+}
+/*test kod
+    elapsedtime(){
+        if (this.running) {
+            return this.elapsed + (Date.now() - this.starttime);
+        }
+        return this.elapsed;
+    }
+    }
+    const timer = new Timer();
+
+    console.log("Starting timer...");
+    timer.start();
+    
+    setTimeout(() => {
+        console.log("Stopping timer...");
+        timer.stop();
+        console.log("Elapsed time:", timer.elapsedtime(), "ms");
+    
+        console.log("Resetting timer...");
+        timer.reset();
+        console.log("Elapsed time after reset:", timer.elapsedtime(), "ms");
+    }, 2000);
+*/ 
