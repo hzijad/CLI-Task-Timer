a8ad58331ccd8c228b6b32e2c9f0a31418c424a2
hasagi33 <123702949+hasagi33@users.noreply.github.com>
2025-01-07 18:49:59 +0100

    Delete Python directory

--- Python/routes/users.py
+++
@@ -1,8 +0,0 @@
-# routes/users.py
-from flask import Blueprint
-
-users_bp = Blueprint('users', __name__)
-
-@users_bp.route('/')
-def users():
-    return 'respond with a resource'
--- Python/utils/utility.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def make_id(length):
-    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    result = ''.join(random.choice(characters) for _ in range(length))
-    return result
